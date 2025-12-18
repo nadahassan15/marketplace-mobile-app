c01cc154e0382b6b424b920d60b1e8f61fcb51bd
nadahassan15 <nada2206712@miuegypt.edu.eg>
2025-12-18 13:57:47 +0200

    fix error in checkout page

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/checkout_provider.dart';
 import 'screens/customer/home_screen.dart';
 import 'screens/customer/checkout_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CheckoutProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
