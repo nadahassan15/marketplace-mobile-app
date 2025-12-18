6d81245e34ec87ecf792947036a368c00757383b
nadahassan15 <nada2206712@miuegypt.edu.eg>
2025-12-18 17:54:25 +0200

    edit in orders data

--- flutter_application_1/lib/providers/order_provider.dart
+++ flutter_application_1/lib/providers/order_provider.dart
@@ -5,32 +5,32 @@ class OrdersProvider extends ChangeNotifier {
   // 🔹 DUMMY DATA (replace with Supabase later)
   final List<OrderModel> _orders = [
     OrderModel(
-      id: 'ORD-001',
-      date: '2025-01-10',
+      id: 'ORD-005',
+      date: '2025-10-06',
       total: 2295,
+      status: 'Processing',
+    ),
+    OrderModel(
+      id: 'ORD-004',
+      date: '2025-05-15',
+      total: 1450,
+      status: 'Shipped',
+    ),
+    OrderModel(
+      id: 'ORD-003',
+      date: '2025-04-22',
+      total: 3200,
       status: 'Delivered',
     ),
     OrderModel(
       id: 'ORD-002',
-      date: '2025-01-18',
-      total: 1450,
-      status: 'Processing',
-    ),
-    OrderModel(
-      id: 'ORD-003',
-      date: '2025-01-22',
-      total: 3200,
-      status: 'Shipped',
-    ),
-    OrderModel(
-      id: 'ORD-004',
-      date: '2025-01-28',
+      date: '2025-02-28',
       total: 980,
       status: 'Cancelled',
     ),
     OrderModel(
-      id: 'ORD-005',
-      date: '2025-02-02',
+      id: 'ORD-001',
+      date: '2025-01-01',
       total: 1875,
       status: 'Delivered',
     ),
